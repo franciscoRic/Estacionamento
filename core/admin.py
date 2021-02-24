6523b07858be637d927e459b9d3029951e0a29d6
Francisco Ricardo <ricardo.souza1910@gmail.com>
2021-02-24 19:49:15 -0400

    Movimento rotativo

---
+++ core/admin.py
@@ -0,0 +1,17 @@
+from django.contrib import admin
+from .models import (
+    Marca,
+    Veiculo,
+    Pessoa,
+    Parametros,
+    MovRotativo
+)
+
+class MovRotativoAdmin(admin.ModelAdmin):
+    list_display = ('checkin', 'checkout', 'valor_hora', 'veiculo', 'horas_total', 'total', 'pago')
+
+admin.site.register(Marca)
+admin.site.register(Veiculo)
+admin.site.register(Pessoa)
+admin.site.register(Parametros)
+admin.site.register(MovRotativo, MovRotativoAdmin)
