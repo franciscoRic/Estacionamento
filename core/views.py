b446c483aec5e203a35c1bafaefbd98265f90176
Francisco Ricardo <ricardo.souza1910@gmail.com>
2021-02-24 23:17:37 -0400

    Create Pessoa

--- core/views.py
+++ core/views.py
@@ -1,6 +1,7 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
 from .models import Pessoa, Veiculo, MovRotativo, Menssalista, MovMenssalista
 
+from core.forms import PessoaForm
 
 def home(request):
     context = {'mensagem': 'Ola Mundo'}
@@ -8,7 +9,15 @@ def home(request):
 
 def lista_pessoas(request):
     pessoas = Pessoa.objects.all()
-    return render(request, 'core/lista_pessoas.html', { 'pessoas': pessoas })
+    form = PessoaForm()
+    data = { 'pessoas': pessoas, 'form': form }
+    return render(request, 'core/lista_pessoas.html', data)
+
+def pessoa_novo(request):
+    form = PessoaForm(request.POST or None)
+    if form.is_valid():
+        form.save()
+    return redirect('core_lista_pessoas')
 
 def lista_veiculos(request):
     veiculos = Veiculo.objects.all()
