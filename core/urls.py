1c9349ec791f8f66e59e97ce4dedbee4c85c797c
Francisco Ricardo <ricardo.souza1910@gmail.com>
2021-02-24 21:33:09 -0400

    Listagem Veiculos MovRotatorio

--- core/urls.py
+++ core/urls.py
@@ -1,9 +1,11 @@
 from django.urls import include, path
-from .views import home, lista_pessoas
+from .views import home, lista_pessoas, lista_veiculos, lista_movrotativo
 
 
 urlpatterns = [
     path('', home, name='core_home'),
     path('pessoas/', lista_pessoas, name='core_lista_pessoas'),
+    path('veiculos/', lista_veiculos, name='core_lista_veiculos'),
+    path('mov_rot_lista/', lista_movrotativo, name='core_mov_rot_lista'),
 
 ]
--- core/views.py
+++ core/views.py
@@ -1,5 +1,6 @@
 from django.shortcuts import render
-from .models import Pessoa
+from .models import Pessoa, Veiculo, MovRotativo
+
 
 def home(request):
     context = {'mensagem': 'Ola Mundo'}
@@ -8,3 +9,11 @@ def home(request):
 def lista_pessoas(request):
     pessoas = Pessoa.objects.all()
     return render(request, 'core/lista_pessoas.html', { 'pessoas': pessoas })
+
+def lista_veiculos(request):
+    veiculos = Veiculo.objects.all()
+    return render(request, 'core/lista_veiculos.html', {'veiculos': veiculos})
+
+def lista_movrotativo(request):
+    movrotativo = MovRotativo.objects.all()
+    return render(request, 'core/lista_mov_rot.html', {'movrotativos': movrotativo})
