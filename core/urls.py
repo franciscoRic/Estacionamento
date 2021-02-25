2aaba2c30983c5d4d88c9a32b225c07e72f872e5
Francisco Ricardo <ricardo.souza1910@gmail.com>
2021-02-24 20:45:36 -0400

    Listagem Pessoa

--- core/urls.py
+++ core/urls.py
@@ -1,8 +1,9 @@
 from django.urls import include, path
-from .views import home
+from .views import home, lista_pessoas
 
 
 urlpatterns = [
     path('', home, name='core_home'),
+    path('pessoas/', lista_pessoas, name='core_lista_pessoas'),
 
 ]
--- core/views.py
+++ core/views.py
@@ -1,7 +1,10 @@
 from django.shortcuts import render
-
+from .models import Pessoa
 
 def home(request):
     context = {'mensagem': 'Ola Mundo'}
     return render(request, 'core/index.html', context)
 
+def lista_pessoas(request):
+    pessoas = Pessoa.objects.all()
+    return render(request, 'core/lista_pessoas.html', { 'pessoas': pessoas })
